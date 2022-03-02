576ddb09b9285e8e64571e1e4605b25d6160af91
Raz Ali <mehraz.hakim@gmail.com>
2022-03-02 16:41:29 -0500

    Add redux and rtk

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import CreateTargetSite from "./feautures/create-link/CreateTargetSite";
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const routing = (
     <React.StrictMode>
-        <Router>
-            <Switch>
-                <Route path="/create">
-                    <CreateTargetSite/>
-                </Route>
-                <Route path="/">
-                    <App/>
-                </Route>
-            </Switch>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route path="/create">
+                        <CreateTargetSite/>
+                    </Route>
+                    <Route path="/">
+                        <App/>
+                    </Route>
+                </Switch>
+            </Router>
+        </Provider>
     </React.StrictMode>
 )
 ReactDOM.render(
