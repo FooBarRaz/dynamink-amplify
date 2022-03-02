2f6339167792f06ca782c4ce979350e6b33d8452
Raz Ali <mehraz.hakim@gmail.com>
2022-03-02 16:42:33 -0500

    reorganize directory for link feature

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import CreateTargetSite from "./feautures/create-link/CreateTargetSite";
+import CreateTargetSite from "./feautures/links/create/CreateTargetSite";
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
