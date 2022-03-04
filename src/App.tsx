d5e6c6bdfc910a3bbfde37ce2045e07182d676fc
Raz Ali <mehraz.hakim@gmail.com>
2022-03-04 15:22:10 -0500

    update project config

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Amplify from 'aws-amplify';
 // Get the aws resources configuration parameters
-import awsconfig from '../aws-exports'
+import awsconfig from './aws-exports'
 Amplify.configure(awsconfig);
 
 function App() {
